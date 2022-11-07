d9ea7a8f3d5967462262033c276a07759b1d5bbd
AV <ar.vafi@gmail.com>
2022-11-07 10:59:05 +0330

    missing file

---
+++ src/Converters/PersianDateTimeToTimeStampConverter.cs
@@ -0,0 +1,42 @@
+﻿using Persian.Plus.DateTime;
+using System.Globalization;
+
+namespace DevUtils.Converters;
+
+public class PersianDateTimeToTimeStampConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value == null)
+            return null;
+
+        return ConvertFrom(value.ToString()).ToString("yyyy-MM-dd HH:mm:ss");
+    }
+
+    private static long ConvertTo(string value)
+    {
+        var d = PersianDateTime.Parse(value);
+        var dd = new DateTimeOffset(d).ToUnixTimeMilliseconds();
+        return dd;
+    }
+
+    private static PersianDateTime ConvertFrom(string value)
+    {
+        var raw = long.Parse(value);
+        if (value.Length > 10)
+        {
+            var t1 = DateTimeOffset.FromUnixTimeMilliseconds(raw).LocalDateTime;
+            return t1;
+        }
+        var t2 = DateTimeOffset.FromUnixTimeSeconds(raw).LocalDateTime;
+        return t2;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value == null)
+            return null;
+
+        return ConvertTo(value.ToString()).ToString();
+    }
+}
